2db78a787e1b2cb5b2f5238019b5a2986b2099e4
AElMehdi <assirelmehdi@gmail.com>
2020-02-22 20:24:54 +0100

    Greeny: Test KStreams using integration utils

--- src/test/scala/kafka/streams/WordsCounterShould.scala
+++ src/test/scala/kafka/streams/WordsCounterShould.scala
@@ -1,15 +1,18 @@
 package kafka.streams
 
-import java.util.Properties
+import java.util.{Properties, Random}
 
-import org.apache.kafka.common.serialization.{LongDeserializer, StringDeserializer, StringSerializer}
-import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.common.serialization.{LongDeserializer, LongSerializer, StringDeserializer, StringSerializer}
 import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.Serdes._
 import org.apache.kafka.streams.scala.StreamsBuilder
-import org.apache.kafka.streams.scala.kstream.KTable
+import org.apache.kafka.streams.scala.kstream.{KStream, KTable}
 import org.apache.kafka.streams.{KeyValue, StreamsConfig, TopologyTestDriver}
 import org.assertj.core.api.Assertions.assertThat
 import org.scalatest.funsuite.AnyFunSuite
+
+import scala.collection.JavaConverters._
+
 
 class WordsCounterShould extends AnyFunSuite {
   var streamsConfig: Properties = {
@@ -30,6 +33,7 @@ class WordsCounterShould extends AnyFunSuite {
       "Hello kafka Streams",
       "All streams lead to kafka",
       "Join Kafka Summit",
+      "Finally it's working!",
     )
 
     val expected = Map(("hello", 1L),
@@ -39,32 +43,48 @@ class WordsCounterShould extends AnyFunSuite {
       ("to", 1L),
       ("join", 1L),
       ("kafka", 3L),
-      ("summit", 1L))
+      ("summit", 1L),
+      ("finally", 1L),
+      ("it", 1L),
+      ("s", 1L),
+      ("working", 1L))
 
     val builder = new StreamsBuilder
 
+    // Create a stream from the "input-topic"
     val textLines: KStream[Array[Byte], String] = builder.stream[Array[Byte], String](inputTopic)
 
+    // Counting logic applied to the stream
     val wordCounts: KTable[String, Long] = textLines
       .flatMapValues(value => value.toLowerCase.split("\\W+"))
       .groupBy((_, word) => word)
       .count()
 
+    // Convert the KTable to a KStream and materialize it to a topic
     wordCounts.toStream.to(outputTopic)
 
 
     val topologyTestDriver = new TopologyTestDriver(builder.build(), streamsConfig)
 
 
-    val input = topologyTestDriver.createInputTopic(inputTopic,
-       null,
-      new StringSerializer)
-    val output = topologyTestDriver.createOutputTopic(outputTopic,
-      new StringDeserializer,
-      new LongDeserializer)
+    try {
+      // Setup input and output topics
+      // TODO: Use a VoidSerializer
+      val input = topologyTestDriver.createInputTopic(inputTopic,
+        new LongSerializer,
+        new StringSerializer)
+      val output = topologyTestDriver.createOutputTopic(outputTopic,
+        new StringDeserializer,
+        new LongDeserializer)
 
-    input.pipeKeyValueList(inputTextLines.map(v => new KeyValue(null, v)).asJava)
+      // Publish the input topics
+      // TODO: A null key should be used instead of a random long.
+      val l = new Random().nextLong()
+      input.pipeKeyValueList(inputTextLines.map(v => new KeyValue(java.lang.Long.valueOf(l), v)).asJava)
+      assertThat(output.readKeyValuesToMap()).isEqualTo(expected.asJava)
+    } finally {
+      topologyTestDriver.close()
+    }
 
-    assertThat(output.readKeyValuesToMap()).isEqualTo(expected)
   }
 }
