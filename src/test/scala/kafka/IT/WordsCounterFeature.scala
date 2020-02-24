7ce06cefc8e507adeca865dffc6547857121b35e
AElMehdi <assirelmehdi@gmail.com>
2020-02-24 21:25:07 +0100

    Add an IT test using testcontainers

---
+++ src/test/scala/kafka/IT/WordsCounterFeature.scala
@@ -0,0 +1,26 @@
+package kafka.IT
+
+import java.util.Properties
+
+import com.dimafeng.testcontainers.{ForAllTestContainer, KafkaContainer}
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.assertj.core.api.Assertions
+import org.scalatest.funsuite.AnyFunSuite
+
+class WordsCounterFeature extends AnyFunSuite with ForAllTestContainer {
+   override val container = KafkaContainer()
+
+  test("Should start a Kafka container") {
+    val properties = new Properties()
+    properties.put("bootstrap.servers", container.bootstrapServers)
+    properties.put("group.id", "consumer-tutorial")
+    properties.put("key.deserializer", classOf[StringDeserializer])
+    properties.put("value.deserializer", classOf[StringDeserializer])
+
+    val kafkaConsumer = new KafkaConsumer[String, String](properties)
+    val topics = kafkaConsumer.listTopics()
+
+    Assertions.assertThat(topics.size()).isGreaterThan(0)
+  }
+}
