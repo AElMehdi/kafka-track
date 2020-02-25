e461a77b883a45a62a9b46278f40f834c3e751de
AElMehdi <assirelmehdi@gmail.com>
2020-02-25 15:08:06 +0100

    Make the producer testable
    Test still not working a LEADER thing causing it to fail

--- src/test/scala/kafka/IT/WordsCounterFeature.scala
+++ src/test/scala/kafka/IT/WordsCounterFeature.scala
@@ -1,10 +1,12 @@
 package kafka.IT
 
-import java.util.Properties
+import java.time.Duration
+import java.util.{Collections, Properties}
 
 import com.dimafeng.testcontainers.{ForAllTestContainer, KafkaContainer}
+import kafka.producer_consumer.WordsProducer
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 import org.assertj.core.api.Assertions
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -25,16 +27,27 @@ class WordsCounterFeature extends AnyFunSuite with ForAllTestContainer {
   }
 
   test("Should produce and consume a hello message") {
-    val properties = new Properties()
-    properties.put("bootstrap.servers", container.bootstrapServers)
-    properties.put("group.id", "consumer-tutorial")
-    properties.put("key.deserializer", classOf[StringDeserializer])
-    properties.put("value.deserializer", classOf[StringDeserializer])
+    val producerProperties = new Properties()
+    producerProperties.put("bootstrap.servers", container.bootstrapServers)
+    producerProperties.put("key.serializer", classOf[StringSerializer])
+    producerProperties.put("value.serializer", classOf[StringSerializer])
 
 
-    val kafkaConsumer = new KafkaConsumer[String, String](properties)
-    val topics = kafkaConsumer.listTopics()
+    val consumerProperties = new Properties()
+    consumerProperties.put("bootstrap.servers", container.bootstrapServers)
+    consumerProperties.put("group.id", "consumer-tutorial")
+    consumerProperties.put("key.deserializer", classOf[StringDeserializer])
+    consumerProperties.put("value.deserializer", classOf[StringDeserializer])
 
-    Assertions.assertThat(topics.size()).isGreaterThan(0)
+
+    WordsProducer.writeToKafka("input-topic", producerProperties, "Hello Kafka")
+
+
+
+    val kafkaConsumer = new KafkaConsumer[String, String](consumerProperties)
+    kafkaConsumer.subscribe(Collections.singletonList("input-topic"))
+    val records = kafkaConsumer.poll(Duration.ofSeconds(1))
+
+    Assertions.assertThat(records.count()).isGreaterThan(0)
   }
 }
