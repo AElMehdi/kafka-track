2988a90cab990f685325f7f1c3f372a6d83bec60
AElMehdi <assirelmehdi@gmail.com>
2020-03-05 21:37:44 +0100

    WIP: Create a command line producer

--- src/main/scala/kafka/producer_consumer/WordsProducer.scala
+++ src/main/scala/kafka/producer_consumer/WordsProducer.scala
@@ -3,13 +3,27 @@ package kafka.producer_consumer
 import java.util.Properties
 
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
 
 object WordsProducer {
 
-   def writeToKafka(topic: String, config: Properties, content: String): Unit = {
-      val producer = new KafkaProducer[String, String](config)
-      val record = new ProducerRecord[String, String](topic, "key", content)
-      producer.send(record)
-      producer.close()
-   }
+  def main(args: Array[String]): Unit = {
+    val producerProperties = new Properties()
+    producerProperties.put("bootstrap.servers", "localhost:9092")
+    producerProperties.put("key.serializer", classOf[StringSerializer])
+    producerProperties.put("value.serializer", classOf[StringSerializer])
+    producerProperties.put("listeners", "PLAINTEXT://0.0.0.0:9092")
+    producerProperties.put("advertised.listeners", "PLAINTEXT://my.public.ip:9092")
+
+    val str = scala.io.StdIn.readLine()
+
+    writeToKafka("quick-start", producerProperties, str)
+  }
+
+  def writeToKafka(topic: String, config: Properties, content: String): Unit = {
+    val producer = new KafkaProducer[String, String](config)
+    val record = new ProducerRecord[String, String](topic, "key", content)
+    producer.send(record)
+    producer.close()
+  }
 }
